5eacaf39e22f891bc95f9580527bf3f843c7a855
chaitanya jonnadula <jonnadulacj57@gmail.com>
2026-05-24 17:53:16 -0400

    added new conversatin with variable

--- Conversations.sh
+++ Conversations.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Trump:Hey Iran we are going to blast you buddy"
-echo "Iran:you can do that buddy"
-echo "Trump:I am serious"
-echo "Iran:we are also serious"
+PERSON1=TRUMP
+PERSON2=IRAN
+
+echo "$PERSON1: Hey $PERSON2 we are going to blast you buddy"
+echo "$PERSON2: you can do that $PERSON1"
+echo "$PERSON1: I am serious $PERSON2"
+echo "$PERSON2: we are also serious $PERSON1"
