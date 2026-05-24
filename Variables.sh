dec337142a03cd19e76fec8474a261e84c32c5d6
chaitanya jonnadula <jonnadulacj57@gmail.com>
2026-05-24 18:06:49 -0400

    new file added with CL args

---
+++ Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2 we are going to blast you buddy"
+echo "$PERSON2: you can do that $PERSON1"
+echo "$PERSON1: I am serious $PERSON2"
+echo "$PERSON2: we are also serious $PERSON1"
