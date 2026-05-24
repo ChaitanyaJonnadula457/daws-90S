85d363f2eacd2c6c24eca7d03c8a39434926ebee
chaitanya jonnadula <jonnadulacj57@gmail.com>
2026-05-24 18:26:05 -0400

    new file added 2nd Variables

--- variables2.sh
+++ variables2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username::"
-Read USER_NAME #this gonna take Input from the user
+read USER_NAME #this gonna take Input from the user
 echo "Your Name is $USER_NAME"
 
 echo "Please enter your Password::"
-Read -s PASSWORD #this gonna take password from the user But silence it
+read -s PASSWORD #this gonna take password from the user But silence it
 echo "your password is $PASSWORD"
