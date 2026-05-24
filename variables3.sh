92253a378d323f22742362c3ad7da2632b907238
chaitanya jonnadula <jonnadulacj57@gmail.com>
2026-05-24 18:52:35 -0400

    new file added 3rd Variables

--- variables3.sh
+++ variables3.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-TIME_STAMP=$(date)
-echo "time is $TIME_STAMP"
+# TIME_STAMP=$(date)
+# echo "time is $TIME_STAMP"
+
+STARTTIME=$(date +%s)
+sleep 10s
+ENDTIME=$(date +%s)
+
+TOTAL_TIME=(($STARTTIME-$ENDTIME))
+
+
+echo "script take $TOTAL_TIME seconds"
